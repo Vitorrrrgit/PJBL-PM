4eb66aae9dcedd96598ed037a2784ada19954cb7
Yash1c <guitrevisan2006@gmail.com>
2025-06-10 22:10:38 -0300

    Atualização do código

--- Coordenador.java
+++ Coordenador.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.io.Serializable;
 
-public class Coordenador extends Usuario {
+public class Coordenador extends Usuario implements Serializable{
     private List<Professor> professores = new ArrayList<>();
 
-    public Coordenador(String nome, int id, int telefone, String email) {
-        super(nome, id, telefone, email);
+    public Coordenador(int id, String tipoUsuario, String nome, String email, String telefone){
+        super(id, nome, email, telefone);
     }
 
     public List<Professor> getProfessores() {
@@ -14,24 +15,11 @@ public class Coordenador extends Usuario {
     } 
 
     public void addProfessor(Professor professores){
-        professores.add(professores);
+        this.professores.add(professores);
     }
 
-    // 1º Função
-    public String getNome() {
-        return nome;
-    }
-    //2º Função
-    public int getId() {
-        return id;
-    }
-    //3º Função
-    public int getTelefone() {
-        return telefone;
-    }
-    //4º Função 
-    public String getEmail() {
-        return email;
+    public void removeProfessor(Professor professores){
+        this.professores.remove(professores);
     }
 
 }
--- Usuario.java
+++ Usuario.java
@@ -3,28 +3,34 @@ public abstract class Usuario {
 
     // atributos usuarios
     private int     id;
-    private String  tipoUsuario;
     private String  nome;
     private String  email;
     private String  telefone;
 
     // construtor usuario
-    public Usuario(int id, String tipoUsuario, String nome, String email, String telefone) {
+    public Usuario(int id, String nome, String email, String telefone) {
         this.id             = id;
-        this.tipoUsuario    = tipoUsuario;
         this.nome           = nome;
         this.email          = email;
         this.telefone       = telefone;
     }
 
     // metodo abstrato
-    public void exibirTipoUsuario() {
-        System.out.println("Tipo do usuario: " + tipoUsuario);
-    }
 
     // getter
+    public int getId(){
+        return id;
+    }
+
     public String getNome() {
         return nome;
     }
 
+    public String getEmail(){
+        return email;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
 }
