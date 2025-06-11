bd7eb69656bbe1f179619eab8f3fff64e6ead273
Yash1c <guitrevisan2006@gmail.com>
2025-06-10 21:09:21 -0300

    Salvando progresso na branch coordenador

--- Coordenador.java
+++ Coordenador.java
@@ -1,28 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Coordenador extends Usuario {
-    private String nome;
-    private int id;
-    private int telefone;
-    private String email;
+    private List<Professor> professores = new ArrayList<>();
 
     public Coordenador(String nome, int id, int telefone, String email) {
-        this.nome = nome;
-        this.id = id;
-        this.telefone = telefone;
-        this.email = email;
+        super(nome, id, telefone, email);
     }
 
+    public List<Professor> getProfessores() {
+        return professores;
+    } 
+
+    public void addProfessor(Professor professores){
+        professores.add(professores);
+    }
+
+    // 1º Função
     public String getNome() {
         return nome;
     }
-
+    //2º Função
     public int getId() {
         return id;
     }
-
+    //3º Função
     public int getTelefone() {
         return telefone;
     }
-
+    //4º Função 
     public String getEmail() {
         return email;
     }
